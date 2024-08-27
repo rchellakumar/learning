509260f6d534fb66e800407faf76a3fec4e920a7
Mr.Chella Kumar <ckchellakumar2003>
2024-08-27 13:23:29 +0530

    FIX: new changes for java and css

--- samplerepo/run.java
+++ samplerepo/run.java
@@ -1,10 +1,17 @@
 package learning.samplerepo;
+import java.util.Scanner;
+
+
 public class run{
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
         int a =35;
         int b=55;
         int c=a+b;
+        int d=scan.nextInt();
+        int e=c/d;
+        scan.close();
         System.out.println("This is a sample file git repo");
-        System.out.println("The answer is: "+c);
+        System.out.println("The answer is: "+c+e);
     }
 }
