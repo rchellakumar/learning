e6f23abdd6c3830a652abd9fd2a1cb77a9b3dbc0
CHELLAKUMAR R <113930639+rchellakumar@users.noreply.github.com>
2024-08-27 12:57:15 +0530

    FIX:: java file updated

--- samplerepo/run.java
+++ samplerepo/run.java
@@ -1,6 +1,10 @@
 package learning.samplerepo;
 public class run{
     public static void main(String[] args) {
+        int a =35;
+        int b=55;
+        int c=a+b;
         System.out.println("This is a sample file git repo");
+        System.out.println("The answer is: "+c)
     }
 }
